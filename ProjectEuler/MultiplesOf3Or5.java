8db1584d329e8cf9fb7f1543ed2239d33258f2a7
Jeff Enriquez <jeff.enriquez.h@gmail.com>
2023-09-04 22:51:28 -0400

    Add "Multiples of 3 or 5"

--- ProjectEuler/MultiplesOf3Or5.java
+++ ProjectEuler/MultiplesOf3Or5.java
@@ -1,68 +1,74 @@
-// https://projecteuler.net/problem=1
+/*
+ * Based on the example: "multiples of 3 and 5 below 1000 equals 233168" we know that shared multiples, such as 15, are only counted once.
+ * This means we need to calculate the following:
+ * 		- all the multiples of 3
+ * 		- all the multiples of 5
+ * 		- all the multiples of (3 * 5)
+ * Then we add the multiples of 3, plus the multiples of 5, and subtract the multiples of (3 * 5).
+ * 		The reason we subtract the multiples of (3 * 5) is because they are counted twice. Once by multiples of 3 and again by multiples of 5. We only want the multiple one time, not two.
+ */
 public class MultiplesOf3Or5 {
 	
 	/*
-     * This solution can calculate any two multiples below a given value
-     * 
-	 * Time Complexity: O(n) where n is the topValue
+	 * Time Complexity: O(x)
 	 * Space Complexity: O(1)
 	 */
-	public static void solution1(long topValue, long firstMultiple, long secondMultiple) {
+	public static void solution1(long x, long m1, long m2) {
 		// Get the sum of the values of the first multiple
-		long firstMultipleSum = 0;
-		for(long i = firstMultiple; i < topValue; i += firstMultiple) {
-			firstMultipleSum += i;
+		long m1Sum = 0;
+		for(long i = m1; i < x; i += m1) {
+			m1Sum += i;
 		}
 		
 		// Get the sum of the values of the second multiple
-		long secondMultipleSum = 0;
-		for(long i = secondMultiple; i < topValue; i += secondMultiple) {
-			secondMultipleSum += i;
+		long m2Sum = 0;
+		for(long i = m2; i < x; i += m2) {
+			m2Sum += i;
 		}
 		
 		// Get the sum of the values of the combined multiples
-		long combinedMultiples = firstMultiple * secondMultiple;
-		long combinedMultiplesSum = 0;
-		for(long i = combinedMultiples; i < topValue; i += combinedMultiples) {
-			combinedMultiplesSum += i;
+		long m3 = m1 * m2;
+		long m3Sum = 0;
+		for(long i = m3; i < x; i += m3) {
+			m3Sum += i;
 		}
 		
-		// Calculate the sum
-		long sum = firstMultipleSum + secondMultipleSum - combinedMultiplesSum;
+		// Calculate the sum of the first and second multiples below "x"
+		long sum = m1Sum + m2Sum - m3Sum;
 		
 		// Print the sum
 		System.out.println(sum);
 	}
 
 	/*
-     * This solution can calculate any two multiples below a given value
-     * 
 	 * Time Complexity: O(1)
 	 * Space Complexity: O(1)
 	 */
-	public static void solution2(long topValue, long firstMultiple, long secondMultiple) {
+	public static void solution2(long x, long m1, long m2) {
 		// We will be using the math formula "Sum of an arithmetic series" to help solve this problem.
 			// (n / 2) * (2a + (n - 1) * d)
 			// n represents the number of terms to be added
 			// a is the first term in the sequence
 			// d is the constant value between terms
 		
-		long combinedMultiple = (firstMultiple * secondMultiple);
+		// Needed for calculating the sum of the first and second multiples below "x"
+		long m3 = (m1 * m2);
 		
-		// we want the numbers below the topValue
-		long maxValue = topValue - 1;
-		// Get the n value for the formula
-		double firstN = maxValue / firstMultiple;
-		double secondN = maxValue / secondMultiple;
-		double combinedN = maxValue / (firstMultiple * secondMultiple);
+		// We only want the numbers below the x, not equal to it
+		long maxValue = x - 1;
 		
-		// Calculate arithmetic series
-		double firstSeries = sumOfArithmeticSeries(firstN, firstMultiple, firstMultiple);
-		double secondSeries = sumOfArithmeticSeries(secondN, secondMultiple, secondMultiple);
-		double combinedSeries = sumOfArithmeticSeries(combinedN, combinedMultiple, combinedMultiple);
+		// Get the n value for the formula. We do not want a fraction.
+		long nForM1 = maxValue / m1;
+		long nForM2 = maxValue / m2;
+		long nForM3 = maxValue / m3;
 		
-		// Calculate sum
-		double sum = firstSeries + secondSeries - combinedSeries;
+		// Calculate the sum of each multiple using the arithmetic series
+		double sumOfM1 = sumOfArithmeticSeries(nForM1, m1, m1);
+		double sumOfM2 = sumOfArithmeticSeries(nForM2, m2, m2);
+		double sumOfM3 = sumOfArithmeticSeries(nForM3, m3, m3);
+		
+		// Calculate the sum of the first and second multiples below "x"
+		double sum = sumOfM1 + sumOfM2 - sumOfM3;
 		
 		// Print the sum
 		System.out.println(
@@ -74,5 +80,10 @@ public class MultiplesOf3Or5 {
 	private static double sumOfArithmeticSeries(double n, double a, double d) {
 		return (n / 2.0) * (2.0 * a + (n - 1) * d);
 	}
-
+	
+	public static void main(String[] args) {
+		solution1(14, 2, 3);
+		// answer = 233168
+		solution2(1000, 3, 5);
+	}
 }
