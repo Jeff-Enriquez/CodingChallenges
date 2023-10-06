da0af68cb79a10b9e2987ec2865b7e28a5903afd
Jeff Enriquez <jeff.enriquez.h@gmail.com>
2023-10-06 16:41:40 -0400

    Add tower breakers solution

---
+++ HackerRank/TowerBreakers/TowerBreakers.java
@@ -0,0 +1,15 @@
+public class TowerBreakers {
+
+    /*
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     */
+    public static int towerBreakers(int n, int m) {
+        // If there are an even number of towers, player 2 wins
+        // If the height of all the towers is 1, player 2 wins
+        if(n % 2 == 0 || m == 1)
+            return 2;
+        return 1;
+    }
+
+}
